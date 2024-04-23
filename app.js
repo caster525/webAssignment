b8ea8897267064e8dc68b82ad0ca23a103f6edbc
KIUKIUTAM <a1998gogo@icloud.com>
2024-04-23 22:33:53 +0800

    update icon

--- app.js
+++
@@ -1,9 +0,0 @@
-let header =document.querySelector("header");
-
-window.addEventListener("scroll",()=>{
-    if(window.pageYOffset !=0){
-        header.style.backgroundColor = "green";
-    }else{
-        header.style.backgroundColor = "#497537";
-    }
-})  
