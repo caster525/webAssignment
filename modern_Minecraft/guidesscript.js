fffd5d209ecb9b719abaf67a92b6916235e22341
KIUKIUTAM <a1998gogo@icloud.com>
2024-04-29 00:44:02 +0800

    guides done

--- modern_Minecraft/guidesscript.js
+++ modern_Minecraft/guidesscript.js
@@ -11,7 +11,6 @@ const btns = document.querySelectorAll(".nav-btn");
 const slides = document.querySelectorAll(".video-slide");
 const contents = document.querySelectorAll(".content");
 
-
 var sliderNav = function(manual){
     btns.forEach((btn)=>{
         btn.classList.remove("active");
@@ -22,6 +21,7 @@ var sliderNav = function(manual){
     contents.forEach((content)=>{
         content.classList.remove("active");
     });
+
 
     btns[manual].classList.add("active");
     slides[manual].classList.add("active");
