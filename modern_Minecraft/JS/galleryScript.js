bd79dded4f9c8dcff937e6a795036ebf3adddc01
lancastercch <130977676+caster525@users.noreply.github.com>
2024-04-29 02:07:15 +0800

    added bg music

--- modern_Minecraft/JS/galleryScript.js
+++ modern_Minecraft/JS/galleryScript.js
@@ -1,16 +1,13 @@
-// create references to the modal...
 var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
 var images = document.getElementsByClassName('myImages');
-// the image in the modal
 var modalImg = document.getElementById("img01");
-// and the caption in the modal
 var captionText = document.getElementById("caption");
+var audio = document.getElementById("bgMusic");
 
-// Go through all of the images with our custom class
+audio.volume = 0.5;
+
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
-  // and attach our click listener for this image.
   img.onclick = function() {
     modal.style.display = "block";
     modalImg.src = this.src;
